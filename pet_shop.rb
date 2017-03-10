2ea41ff67d7b857eb779134880a3d375879e07e2
Paule AD <akramaite.paule@gmail.com>
2017-03-10 19:57:39 +0000

    pet by name works

--- pet_shop.rb
+++ pet_shop.rb
@@ -31,3 +31,12 @@ def pets_by_breed(shop, breed)
   end
   return pets_array
 end
+
+def find_pet_by_name(shop, name)
+  for pet in shop[:pets]
+    if pet[:name] == name
+      return pet
+    end
+  end
+  return nil
+end
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -115,20 +115,20 @@ class TestPetShop < Minitest::Test
     assert_equal(2, pets.count)
   end
 
-  # def test_all_pets_by_breed__not_found
-  #   pets = pets_by_breed(@pet_shop, "Dalmation")
-  #   assert_equal(0, pets.count)
-  # end
+  def test_all_pets_by_breed__not_found
+    pets = pets_by_breed(@pet_shop, "Dalmation")
+    assert_equal(0, pets.count)
+  end
 
-  # def test_find_pet_by_name__returns_pet
-  #   pet = find_pet_by_name(@pet_shop, "Arthur")
-  #   assert_equal("Arthur", pet[:name])
-  # end
-
-  # def test_find_pet_by_name__returns_nil
-  #   pet = find_pet_by_name(@pet_shop, "Fred")
-  #   assert_equal(nil, pet)
-  # end
+  def test_find_pet_by_name__returns_pet
+    pet = find_pet_by_name(@pet_shop, "Arthur")
+    assert_equal("Arthur", pet[:name])
+  end
+#running the test said "use assert_nil"
+  def test_find_pet_by_name__returns_nil
+    pet = find_pet_by_name(@pet_shop, "Fred")
+    assert_nil(pet)
+  end
 
   # def test_remove_pet_by_name
   #   remove_pet_by_name(@pet_shop, "Arthur")
